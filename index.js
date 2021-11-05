bc6b34697974c431fd06c223933ed753596956b1
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2021-11-05 10:07:17 -0700

    Assigment completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('main');
+main.remove('main');
+
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'victory';
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
